465bcd8fd75a1cdf70389b0190eae0637df473f5
annieluo477 <131917344+annieluo477@users.noreply.github.com>
2023-05-02 17:16:56 -0700

    Add files via upload

---
+++ cancelbooking.sql
@@ -0,0 +1,25 @@
+-- this sql file is used to cancel bookings from the little lemon database
+
+USE littlelemondb;
+DROP PROCEDURE IF EXISTS CancelBooking;
+DELIMITER //
+
+CREATE PROCEDURE CancelBooking (IN booking_id INT)
+BEGIN
+START TRANSACTION;
+IF NOT EXISTS(SELECT 1 FROM Booking WHERE (BookingID = booking_id))
+THEN
+SELECT CONCAT("Booking " , booking_id, " does not exist.") AS `Message`;
+ROLLBACK;
+ELSE
+DELETE FROM Booking WHERE BookingID = booking_id;
+SELECT CONCAT("Booking ", booking_id ," cancelled.") AS `Confirmation`; 
+
+COMMIT;
+END IF;
+END //
+
+
+DELIMITER ;
+
+Call CancelBooking(6);
